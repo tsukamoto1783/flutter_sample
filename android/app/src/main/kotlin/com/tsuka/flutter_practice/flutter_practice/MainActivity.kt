75d89bf32f1d5cb51d861bdfb0a04860fd1cb4c6
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-02-09 21:00:24 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/tsuka/flutter_practice/flutter_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tsuka.flutter_practice.flutter_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/next_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget{
+  NextPage(this.name);
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("next Page now"),
+      ),
+      body: Container(
+        height: double.infinity,
+        color: Colors.red,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(name),
+            Center(
+              child: RaisedButton(
+                child: Text("Back!"),
+                onPressed: (){
+                  Navigator.pop(context,"hhhhhh");
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
