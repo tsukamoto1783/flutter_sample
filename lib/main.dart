1963c8366517875a764a6a387d35d546ff2baac3
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-02-11 08:02:19 +0900

    Sample using "provider".

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,46 @@
 import 'package:flutter/material.dart';
-
-import 'next_page.dart';
+import 'package:flutter_practice/main_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      title: "Flutter App",
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+        appBar: AppBar(
+          title: Text("hogehoge"),
+        ),
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  String text = "next";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[Icon(Icons.add)],
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text(text),
-          onPressed: () async {
-            // ここを押すと反応するBtn
-            final result = await Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => NextPage("hogehoge")
+        body: Consumer<MainModel>(builder: (context, model, child) {
+            return Center(
+              child: Column(
+                children: [
+                  Text(
+                    model.my_text,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text("Btn"),
+                    onPressed: (){
+                    // ここでなにかの処理
+                      model.change_cample_text();
+                    },
+                  )
+                ],
               ),
             );
-            print(result);
-          }
+        }),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
