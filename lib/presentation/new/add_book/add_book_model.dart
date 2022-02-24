1470232b391bac96a2da9baa6814571b41b68446
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-02-24 20:21:33 +0900

    Image registration function

--- lib/presentation/new/add_book/add_book_model.dart
+++ lib/presentation/new/add_book/add_book_model.dart
@@ -1,11 +1,43 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddBookModel extends ChangeNotifier {
   String? title;
   String? author;
+  File? imageFile;
+  String? imgURL;
+  bool isLoading = false;
 
+  final picker = ImagePicker();
+
+  // Loading function
+  void startLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  void endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
+
+  // Acquire image from device
+  Future pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      imageFile = File(pickedFile.path);
+      notifyListeners();
+    }
+  }
+
+  //　Register items in firebase
   Future addBook() async {
+    final doc = FirebaseFirestore.instance.collection('books').doc();
+
     if (title == null || title == "") {
       throw 'タイトルが入力されていません';
     }
@@ -14,10 +46,18 @@ class AddBookModel extends ChangeNotifier {
       throw '著者が入力されていません';
     }
 
-    // firestoreに追加
-    await FirebaseFirestore.instance.collection('books').add({
+    if (imageFile != null) {
+      final task = await FirebaseStorage.instance
+          .ref('books/${doc.id}')
+          .putFile(imageFile!);
+      imgURL = await task.ref.getDownloadURL();
+    }
+
+    // dding to Firebase
+    await doc.set({
       'title': title,
       'author': author,
+      'imgURL': imgURL,
     });
   }
 }
