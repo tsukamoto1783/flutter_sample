79f6c8d6082413255b0cb5711281f9bc5d9bf996
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-03-14 19:13:17 +0900

    ToDo app Sample

--- lib/presentation/State_test/state.dart
+++ lib/presentation/State_test/state.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class StateTestPage extends StatefulWidget {
+  StateTestPage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _StateTestPageState createState() => _StateTestPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _StateTestPageState extends State<StateTestPage> {
   int _counter = 0;
 
   void _incrementCounter() {
---
+++ lib/presentation/State_test/todo_main.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'todo.dart';
+
+/// Some keys used for testing
+final addTodoKey = UniqueKey();
+final activeFilterKey = UniqueKey();
+final completedFilterKey = UniqueKey();
+final allFilterKey = UniqueKey();
+
+/// Creates a [TodoList] and initialise it with pre-defined values.
+/// ([TodoList]を作成し、あらかじめ定義された値で初期化する。)
+/// We are using [StateNotifierProvider] here as a `List<Todo>` is a complex
+/// (ここでは、`List<Todo>` が複雑なため、[StateNotifierProvider] を使用しています。)
+/// object, with advanced business logic like how to edit a todo.
+/// (オブジェクトで、Todoの編集方法などの高度なビジネスロジックを持ちます。)
+final todoListProvider = StateNotifierProvider<TodoList, List<Todo>>((ref) {
+  return TodoList(const [
+    Todo(id: 'todo-0', description: 'hi'),
+    Todo(id: 'todo-1', description: 'hello'),
+    Todo(id: 'todo-2', description: 'bonjour'),
+  ]);
+});
+
+/// TODOのリストをフィルタリングするさまざまな方法
+enum TodoListFilter {
+  all,
+  active,
+  completed,
+}
+
+/// 現在アクティブなフィルターです。
+/// 操作するための派手なロジックがないため、ここでは[StateProvider]を使用します。
+/// 単なるenumであるため、その値はありません。
+final todoListFilter = StateProvider((_) => TodoListFilter.all);
+
+/// 未完了のTODOの数
+/// 複数のウィジェットが未完了の ToDo 数を読み込もうとしたときでも,
+/// (todo-listが変更されるまで）一度だけ計算されます。
+/// これは、Todo-Listが変更され、かつ,(Todoの編集時など)未完了のTodoの数は変わりません
+final uncompletedTodosCount = Provider<int>((ref) {
+  return ref.watch(todoListProvider).where((todo) => !todo.completed).length;
+});
+
+/// [todoListFilter]を適用した後のTodoのリスト。
+/// これも[プロバイダ]を使用して、フィルタリングされたリストの再計算を避けるためです。
+/// filter of または todo-list が更新される。
+final filteredTodos = Provider<List<Todo>>((ref) {
+  final filter = ref.watch(todoListFilter);
+  final todos = ref.watch(todoListProvider);
+
+  switch (filter) {
+    case TodoListFilter.completed:
+      return todos.where((todo) => todo.completed).toList();
+    case TodoListFilter.active:
+      return todos.where((todo) => !todo.completed).toList();
+    case TodoListFilter.all:
+      return todos;
+  }
+});
+
+class RiverpodTodo extends HookConsumerWidget {
+  const RiverpodTodo({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Home(),
+//     );
+//   }
+// }
+//
+// class Home extends HookConsumerWidget {
+//   const Home({Key? key}) : super(key: key);
+//
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todos = ref.watch(filteredTodos);
+    final newTodoController = useTextEditingController();
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          children: [
+            const Title(),
+            TextField(
+              key: addTodoKey,
+              controller: newTodoController,
+              decoration: const InputDecoration(
+                labelText: 'What needs to be done?',
+              ),
+              onSubmitted: (value) {
+                ref.read(todoListProvider.notifier).add(value);
+                newTodoController.clear();
+              },
+            ),
+            const SizedBox(height: 42),
+            const Toolbar(),
+            if (todos.isNotEmpty) const Divider(height: 0),
+            for (var i = 0; i < todos.length; i++) ...[
+              if (i > 0) const Divider(height: 0),
+              Dismissible(
+                key: ValueKey(todos[i].id),
+                onDismissed: (_) {
+                  ref.read(todoListProvider.notifier).remove(todos[i]);
+                },
+                child: ProviderScope(
+                  overrides: [
+                    _currentTodo.overrideWithValue(todos[i]),
+                  ],
+                  child: const TodoItem(),
+                ),
+              )
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Toolbar extends HookConsumerWidget {
+  const Toolbar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final filter = ref.watch(todoListFilter);
+
+    Color? textColorFor(TodoListFilter value) {
+      return filter == value ? Colors.blue : Colors.black;
+    }
+
+    return Material(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text(
+              '${ref.watch(uncompletedTodosCount).toString()} items left',
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Tooltip(
+            key: allFilterKey,
+            message: 'All todos',
+            child: TextButton(
+              onPressed: () =>
+                  ref.read(todoListFilter.notifier).state = TodoListFilter.all,
+              style: ButtonStyle(
+                visualDensity: VisualDensity.compact,
+                foregroundColor:
+                    MaterialStateProperty.all(textColorFor(TodoListFilter.all)),
+              ),
+              child: const Text('All'),
+            ),
+          ),
+          Tooltip(
+            key: activeFilterKey,
+            message: 'Only uncompleted todos',
+            child: TextButton(
+              onPressed: () => ref.read(todoListFilter.notifier).state =
+                  TodoListFilter.active,
+              style: ButtonStyle(
+                visualDensity: VisualDensity.compact,
+                foregroundColor: MaterialStateProperty.all(
+                  textColorFor(TodoListFilter.active),
+                ),
+              ),
+              child: const Text('Active'),
+            ),
+          ),
+          Tooltip(
+            key: completedFilterKey,
+            message: 'Only completed todos',
+            child: TextButton(
+              onPressed: () => ref.read(todoListFilter.notifier).state =
+                  TodoListFilter.completed,
+              style: ButtonStyle(
+                visualDensity: VisualDensity.compact,
+                foregroundColor: MaterialStateProperty.all(
+                  textColorFor(TodoListFilter.completed),
+                ),
+              ),
+              child: const Text('Completed'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'todos',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Color.fromARGB(38, 47, 47, 247),
+        fontSize: 100,
+        fontWeight: FontWeight.w100,
+        fontFamily: 'Helvetica Neue',
+      ),
+    );
+  }
+}
+
+/// A provider which exposes the [Todo] displayed by a [TodoItem].
+///
+/// By retreiving the [Todo] through a provider instead of through its
+/// constructor, this allows [TodoItem] to be instantiated using the `const` keyword.
+///
+/// This ensures that when we add/remove/edit todos, only what the
+/// impacted widgets rebuilds, instead of the entire list of items.
+
+/// [TodoItem]が表示する[Todo]を公開するプロバイダです。
+///
+/// [Todo]の取得をプロバイダを通して行うことで、[Todo]をその
+/// [TodoItem] を `const` キーワードを使ってインスタンス化することができます。
+///
+/// これにより、TODOの追加・削除・編集を行った際に、そのTODOに対応する
+/// アイテムのリスト全体ではなく、影響を受けるウィジェットがリビルドされます。
+
+final _currentTodo = Provider<Todo>((ref) => throw UnimplementedError());
+
+class TodoItem extends HookConsumerWidget {
+  const TodoItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todo = ref.watch(_currentTodo);
+    final itemFocusNode = useFocusNode();
+    // listen to focus chances
+    useListenable(itemFocusNode);
+    final isFocused = itemFocusNode.hasFocus;
+
+    final textEditingController = useTextEditingController();
+    final textFieldFocusNode = useFocusNode();
+
+    return Material(
+      color: Colors.white,
+      elevation: 6,
+      child: Focus(
+        focusNode: itemFocusNode,
+        onFocusChange: (focused) {
+          if (focused) {
+            textEditingController.text = todo.description;
+          } else {
+            // Commit changes only when the textfield is unfocused, for performance
+            ref
+                .read(todoListProvider.notifier)
+                .edit(id: todo.id, description: textEditingController.text);
+          }
+        },
+        child: ListTile(
+          onTap: () {
+            itemFocusNode.requestFocus();
+            textFieldFocusNode.requestFocus();
+          },
+          leading: Checkbox(
+            value: todo.completed,
+            onChanged: (value) =>
+                ref.read(todoListProvider.notifier).toggle(todo.id),
+          ),
+          title: isFocused
+              ? TextField(
+                  autofocus: true,
+                  focusNode: textFieldFocusNode,
+                  controller: textEditingController,
+                )
+              : Text(todo.description),
+        ),
+      ),
+    );
+  }
+}
