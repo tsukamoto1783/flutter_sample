2dbbdfada9e9b5d03d90277e24e31418c044e3df
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-03-21 09:27:24 +0900

    README branch for images

--- README.md
+++
@@ -1,5 +0,0 @@
-# flutter_sample
-A sample application created while experimenting with the features of Flutter.
-
-Implemented a simple CRUD function using Firebase.
-
--- android/app/src/main/kotlin/com/tsuka/flutter_practice/flutter_practice/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tsuka.flutter_practice.flutter_practice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
