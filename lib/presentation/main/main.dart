744d4b63b5975af4ad0a1cc55d5f6ca7e0790dc7
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-04-28 20:02:36 +0900

    Picker sample code

--- lib/presentation/main/main.dart
+++ lib/presentation/main/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/BookListRiverpodVer/book_list/riv_book_list_page.dart';
+import 'package:flutter_practice/presentation/test_widget/Picker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import '../Riverpod_sample/Riverpod_menu.dart';
@@ -44,31 +45,38 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        children: [
-          TextBtnNextPage(
-              btn_text: "To: BookListPage", page_name: BookListPageNew()),
-          TextBtnNextPage(
-              btn_text: "To: BookListPage Riverpod Ver",
-              page_name: RivBookListPage()),
-          TextBtnNextPage(btn_text: "dropdwon1", page_name: DropdownPage1()),
-          TextBtnNextPage(btn_text: "dropdwon2", page_name: DropdownPage2()),
-          TextBtnNextPage(
-              btn_text: "StatelfulWidget",
-              page_name: StateTestPage(title: "test StatelessWidget")),
-          TextBtnNextPage(
-              btn_text: "StatefulWidget_2", page_name: MyHomePage_2()),
-          TextBtnNextPage(
-              btn_text: "Riverpod Sample Counter", page_name: RiverpodMenu()),
-          TextBtnNextPage(
-              btn_text: "Riverpod Sample ToDo", page_name: RiverpodTodo()),
-          TextBtnNextPage(
-              btn_text: "DateTimeSample", page_name: DateTimeSample()),
-          TextBtnNextPage(btn_text: "fl_chart_1", page_name: BarChartSample7()),
-          TextBtnNextPage(btn_text: "fl_chart_2", page_name: BarChartSample3()),
-          TextBtnNextPage(
-              btn_text: "firestore_get_sample", page_name: Firesoter_sample()),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextBtnNextPage(
+                btn_text: "To: BookListPage", page_name: BookListPageNew()),
+            TextBtnNextPage(
+                btn_text: "To: BookListPage Riverpod Ver",
+                page_name: RivBookListPage()),
+            TextBtnNextPage(btn_text: "dropdwon1", page_name: DropdownPage1()),
+            TextBtnNextPage(btn_text: "dropdwon2", page_name: DropdownPage2()),
+            TextBtnNextPage(
+                btn_text: "StatelfulWidget",
+                page_name: StateTestPage(title: "test StatelessWidget")),
+            TextBtnNextPage(
+                btn_text: "StatefulWidget_2", page_name: MyHomePage_2()),
+            TextBtnNextPage(
+                btn_text: "Riverpod Sample Counter", page_name: RiverpodMenu()),
+            TextBtnNextPage(
+                btn_text: "Riverpod Sample ToDo", page_name: RiverpodTodo()),
+            TextBtnNextPage(
+                btn_text: "DateTimeSample", page_name: DateTimeSample()),
+            TextBtnNextPage(
+                btn_text: "fl_chart_1", page_name: BarChartSample7()),
+            TextBtnNextPage(
+                btn_text: "fl_chart_2", page_name: BarChartSample3()),
+            TextBtnNextPage(
+                btn_text: "firestore_get_sample",
+                page_name: Firesoter_sample()),
+            TextBtnNextPage(
+                btn_text: "Picker_sample", page_name: PickerSample()),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/presentation/test_widget/Picker.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PickerSample extends StatefulWidget {
+  const PickerSample({Key? key}) : super(key: key);
+  final String title = 'test';
+
+  @override
+  State<PickerSample> createState() => _PickerSampleState();
+}
+
+class _PickerSampleState extends State<PickerSample> {
+  String _selectedItem = 'none';
+  final List<String> _items = [
+    'item1',
+    'item2',
+    'item3',
+    'item4',
+    'item5',
+    'item6',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                _showModalPicker(context);
+              },
+              child: const Text('Show Picker'),
+            ),
+            Text(_selectedItem)
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showModalPicker(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: MediaQuery.of(context).size.height / 3,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: CupertinoPicker(
+              itemExtent: 40,
+              children: _items.map(_pickerItem).toList(),
+              onSelectedItemChanged: _onSelectedItemChanged,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _pickerItem(String str) {
+    return Text(
+      str,
+      style: const TextStyle(fontSize: 32),
+    );
+  }
+
+  void _onSelectedItemChanged(int index) {
+    setState(() {
+      _selectedItem = _items[index];
+    });
+  }
+}
